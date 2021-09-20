8a74df7d6ea88b18511c0a711c1b73c2d90b820b
ian038 <ianphua3@gmail.com>
2021-09-20 15:14:36 -0400

    Added styling and navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import './App.css'
+import { Switch, Route, Link } from 'react-router-dom'
+import { Layout, Typography, Space } from 'antd'
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+
+      </div>
+      <div className="footer">
+
+      </div>
     </div>
   );
 }
